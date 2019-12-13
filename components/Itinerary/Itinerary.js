4cd6ec0cf55ec8da3a04f1d032a403fe6f0457be
team3mnd <mindhubteam3@gmail.com>
2019-12-13 16:46:49 -0300

    ahi vamos como dice j balvin

--- components/Itinerary/Itinerary.js
+++ components/Itinerary/Itinerary.js
@@ -1,16 +1,13 @@
 import React, { Component } from "react";
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
-import "./Itinerary.css";
+//import "./Itinerary.css";
 import { connect } from "react-redux";
-import { postFavourites, getAllFavourites } from '../../store/actions/favouriteActions';
+//import { postFavourites, getAllFavourites } from '../../store/actions/favouriteActions';
 
-import Card from "./card.js";
+//import Card from "./card.js";
+import { View, Text,Image, Button } from "react-native";
 
 // necesario para corazon
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 // fin corazon
 
 class Itinerary extends Component {
@@ -22,117 +19,84 @@ class Itinerary extends Component {
     favourite: false
   };
 
-  async componentDidMount() {
-    // necesario para corazon
-    const token = localStorage.getItem('token');
-    const tokenDecoded = jwt.decode(token);
-    // console.log(tokenDecoded)
-    this.setState({
-      itinerary: this.props.itinerary,
-      hashtags: this.props.itinerary.hashtags
-    });
-    if (tokenDecoded) {
-      const userId = tokenDecoded.id
-      await this.setState({ userId })
-      /* this.setState({
+  /*   async componentDidMount() {
+      // necesario para corazon
+      const token = localStorage.getItem('token');
+      const tokenDecoded = jwt.decode(token);
+      this.setState({
         itinerary: this.props.itinerary,
         hashtags: this.props.itinerary.hashtags
-      }); */
-      this.props.getFavourites(this.state.userId);
+      });
+      if (tokenDecoded) {
+        const userId = tokenDecoded.id
+        await this.setState({ userId })
+        this.props.getFavourites(this.state.userId);
+      }
     }
-  }
-
-  async componentDidUpdate(prevProps) {
-    if (this.props.favourites !== prevProps.favourites) {
-      /* this.setState({
-        listItinerary: this.props.favourites
-      }) */
-
-      if (this.props.favourites.length) {
-        // console.log("this.props.favourites", this.props.favourites);
-        let favArray = this.props.favourites
-        for (let i = 0; i < favArray.length; i++) {
-          if (favArray[i]._id === this.state.itinerary._id) {
-            this.setState({
-              favourite: true
-            })
-            break
-          }
-          else {
-            this.setState({
-              favourite: false
-            })
+  
+    async componentDidUpdate(prevProps) {
+      if (this.props.favourites !== prevProps.favourites) {
+        if (this.props.favourites.length) {
+          let favArray = this.props.favourites
+          for (let i = 0; i < favArray.length; i++) {
+            if (favArray[i]._id === this.state.itinerary._id) {
+              this.setState({
+                favourite: true
+              })
+              break
+            }
+            else {
+              this.setState({
+                favourite: false
+              })
+            }
           }
         }
-      }
-      else {
-        this.setState({
-          favourite: false
-        })
+        else {
+          this.setState({
+            favourite: false
+          })
+        }
       }
     }
-  }
-
-  async onClickFunc() {
-    await this.props.addOrDelFavourite(this.state.userId, this.state.itinerary._id)
-    await this.props.getFavourites(this.state.userId)
-  }
+  
+    async onClickFunc() {
+      await this.props.addOrDelFavourite(this.state.userId, this.state.itinerary._id)
+      await this.props.getFavourites(this.state.userId)
+    } */
 
   render() {
     const { itinerary, expand, hashtags, userId } = this.state;
     return (
-      <div className="containerItinerary" key={itinerary._id}>
-        <div className="wrapperItinerary">
-          <div className="profilePicture">
-            <img src={itinerary.pictureId} alt="imageProfile" id="imageProfile" />
-            <p className='text-break'>{itinerary.author}</p>
-          </div>
-          <div className="infoItinerary">
-            <div className='d-flex flex-row'>
-              <div className="col-9 p-0 pt-3 pl-3">
-                <p className='font-weight-bold'>{itinerary.title}</p>
-              </div>
+      <View  key={itinerary._id}>
+        <View >
+          <View>
+            <Image source={{uri:itinerary.pictureId}} />
+            <Text>{itinerary.author}</Text>
+          </View>
+          <View>
+            <View>
+              <View >
+                <Text>{itinerary.title}</Text>
+              </View>
 
-              <div className="col-3">
-                {userId ?
-                  <FormControlLabel
-                    control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} value="checkedH"
-                      onClick={() => this.onClickFunc()}
-                      checked={this.state.favourite} />} />
-                  :
-                  <FormControlLabel
-                    control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} value="checkedH"
-                      onClick={() => console.log('clicked')}
-                      disabled
-                    />} />
-                }
-              </div>
-            </div>
+            </View>
 
-            <div className="inlineInfo">
-              <span>Likes: {itinerary.rating}</span>
-              <span>Duration: {itinerary.duration}</span>
-              <span>Price: {itinerary.price}</span>
-            </div>
-
-            <div className="w-100 d-flex flex-row flex-wrap">
-              {hashtags.map((hash, i) => (
-                <span key={i} className="badge badge-secondary m-1">{hash}</span>
-              ))}
-            </div>
-          </div>
-        </div>
-        <div className="containerActivities">
-          <div className="d-flex justify-content-center pb-2">
-            <button className="btn btn-outline-info" onClick={() => this.setState({ expand: !this.state.expand })}>
-              View Activities
-          </button>
-          </div>
-          <div className="containerActivitiesExpand">
-            {expand && <Card className="viewsImg" Activities={itinerary.activities} comments={itinerary.comments} _id={itinerary._id} />}
-          </div>
-        </div>
-      </div>
+            <View>
+              <Text>Likes: {itinerary.rating}</Text>
+              <Text>Duration: {itinerary.duration}</Text>
+              <Text>Price: {itinerary.price}</Text>
+            </View>
+          </View>
+        </View>
+        <View >
+          <View>
+            <Button onPress={() => this.setState({ expand: !this.state.expand })} title='View Activities' />
+          </View>
+          <View>
+          </View>
+        </View>
+      </View>
     );
   }
 }
@@ -140,7 +104,7 @@ class Itinerary extends Component {
 
 const mapStateToProps = state => {
   return {
-    favourites: state.favouriteReducer.favourites
+    //favourites: state.favouriteReducer.favourites
   };
 };
 
--- components/store/reducers/rootReducer.js
+++ components/store/reducers/rootReducer.js
@@ -1,17 +1,15 @@
 import { combineReducers } from "redux";
 import cityReducer from "./cityReducer";
 import sesionReducer from "./sesionReducer";
-/* import itineraryReducer from "./itineraryReducer";
-import commentReducer from "./commentReducer";
-import favouriteReducer from "./favouriteReducer"; */
+import itineraryReducer from "./itineraryReducer";
+/* import commentReducer from "./commentReducer";
+import favouriteReducer from "./favouriteReducer";  */
 
 
 const rootReducer = combineReducers({
   cityReducer,
-  sesionReducer/* ,
-  itineraryReducer,
-  commentReducer,
-  favouriteReducer */
+  sesionReducer,
+  itineraryReducer
 });
 
 export default rootReducer;
