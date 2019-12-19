8790eef57800b2b0228e7165b6b861456a9945c8
team3mnd <mindhubteam3@gmail.com>
2019-12-19 17:08:07 -0300

    medio funca google

--- components/login/login.js
+++ components/login/login.js
@@ -4,6 +4,7 @@ import ModalError from '../modalError/modalError'
 import { Button } from 'react-native-elements';
 import { getAccess, clearErrors } from '../store/actions/sesionActions.js';
 import { connect } from "react-redux";
+import * as Google from 'expo-google-app-auth';
 
 class Login extends Component {
   state = {
@@ -39,8 +40,8 @@ class Login extends Component {
       password: this.state.password,
       useGoogle: false
     };
-     this.props.login(user)
-    if(! this.props.errors){
+    this.props.login(user)
+    if (!this.props.errors) {
       this.redirect()
     }
     if (this.props.errors) {
@@ -62,6 +63,15 @@ class Login extends Component {
     this.setState({
       mostrarErrores: false
     })
+  }
+
+  async SignIn() {
+    const result = await Google.logInAsync({
+      androidClientId: '748277599795-j3v5pk26p9soo2v87otpme8gphq385hb.apps.googleusercontent.com',
+      scopes: ['profile', 'email']
+    });
+
+    console.log(result)
   }
 
   render() {
@@ -89,9 +99,12 @@ class Login extends Component {
             <View style={styles.buttonOKContainer}>
               <Button
                 title='OK'
-                onPress={e => this.obtieneLogin(e)} />
+                onPress={this.obtieneLogin} />
+              <Button
+                title='google'
+                onPress={this.SignIn} />
             </View>
-            
+
           </View>
           <View style={styles.buttonRETContainer}>
             <Button title='return'
